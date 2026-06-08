c9d2a9a4439c1a5ee59a883150385241f2acd6eb
cmartinussen <your-email@example.com>
2026-06-08 11:25:42 +0200

    Adjust app icon sizing for taskbar

--- generate-icons.ps1
+++ generate-icons.ps1
@@ -1,9 +1,8 @@
 Add-Type -AssemblyName System.Drawing
 
-function New-TomatoIcon {
+function New-TomatoBitmap {
     param(
-        [int]$Size,
-        [string]$OutputPath
+        [int]$Size
     )
 
     $bitmap = New-Object System.Drawing.Bitmap $Size, $Size
@@ -13,48 +12,49 @@ function New-TomatoIcon {
         $graphics.SmoothingMode = [System.Drawing.Drawing2D.SmoothingMode]::AntiAlias
         $graphics.Clear([System.Drawing.Color]::Transparent)
 
-        $bodyRect = New-Object System.Drawing.RectangleF ($Size * 0.125), ($Size * 0.19), ($Size * 0.75), ($Size * 0.75)
+        $bodyRect = New-Object System.Drawing.RectangleF ($Size * 0.07), ($Size * 0.15), ($Size * 0.86), ($Size * 0.80)
         $bodyBrush = New-Object System.Drawing.Drawing2D.LinearGradientBrush (
             [System.Drawing.PointF]::new($bodyRect.Left, $bodyRect.Top),
             [System.Drawing.PointF]::new($bodyRect.Right, $bodyRect.Bottom),
             [System.Drawing.ColorTranslator]::FromHtml('#ff5a45'),
             [System.Drawing.ColorTranslator]::FromHtml('#c92a1c')
         )
-        $outlinePen = New-Object System.Drawing.Pen ([System.Drawing.Color]::FromArgb(120, 159, 31, 22), ($Size * 0.035))
+        $outlinePen = New-Object System.Drawing.Pen ([System.Drawing.Color]::FromArgb(120, 159, 31, 22), ($Size * 0.03))
 
         $leafColorLight = [System.Drawing.ColorTranslator]::FromHtml('#2fbf65')
         $leafColorDark = [System.Drawing.ColorTranslator]::FromHtml('#167a42')
         $leafBrush = New-Object System.Drawing.Drawing2D.LinearGradientBrush (
-            [System.Drawing.PointF]::new($Size * 0.25, $Size * 0.08),
-            [System.Drawing.PointF]::new($Size * 0.75, $Size * 0.33),
+            [System.Drawing.PointF]::new($Size * 0.19, $Size * 0.03),
+            [System.Drawing.PointF]::new($Size * 0.81, $Size * 0.30),
             $leafColorLight,
             $leafColorDark
         )
         $leafPoints = [System.Drawing.PointF[]]@(
-            [System.Drawing.PointF]::new($Size * 0.31, $Size * 0.33),
-            [System.Drawing.PointF]::new($Size * 0.42, $Size * 0.27),
-            [System.Drawing.PointF]::new($Size * 0.48, $Size * 0.12),
-            [System.Drawing.PointF]::new($Size * 0.52, $Size * 0.12),
-            [System.Drawing.PointF]::new($Size * 0.58, $Size * 0.27),
-            [System.Drawing.PointF]::new($Size * 0.69, $Size * 0.33),
-            [System.Drawing.PointF]::new($Size * 0.75, $Size * 0.19),
-            [System.Drawing.PointF]::new($Size * 0.78, $Size * 0.19),
-            [System.Drawing.PointF]::new($Size * 0.72, $Size * 0.40),
-            [System.Drawing.PointF]::new($Size * 0.50, $Size * 0.44),
-            [System.Drawing.PointF]::new($Size * 0.28, $Size * 0.40),
-            [System.Drawing.PointF]::new($Size * 0.22, $Size * 0.19),
-            [System.Drawing.PointF]::new($Size * 0.25, $Size * 0.19)
+            [System.Drawing.PointF]::new($Size * 0.24, $Size * 0.30),
+            [System.Drawing.PointF]::new($Size * 0.38, $Size * 0.24),
+            [System.Drawing.PointF]::new($Size * 0.46, $Size * 0.05),
+            [System.Drawing.PointF]::new($Size * 0.54, $Size * 0.05),
+            [System.Drawing.PointF]::new($Size * 0.62, $Size * 0.24),
+            [System.Drawing.PointF]::new($Size * 0.76, $Size * 0.30),
+            [System.Drawing.PointF]::new($Size * 0.83, $Size * 0.10),
+            [System.Drawing.PointF]::new($Size * 0.87, $Size * 0.10),
+            [System.Drawing.PointF]::new($Size * 0.79, $Size * 0.39),
+            [System.Drawing.PointF]::new($Size * 0.50, $Size * 0.45),
+            [System.Drawing.PointF]::new($Size * 0.21, $Size * 0.39),
+            [System.Drawing.PointF]::new($Size * 0.13, $Size * 0.10),
+            [System.Drawing.PointF]::new($Size * 0.17, $Size * 0.10)
         )
-        $stemPen = New-Object System.Drawing.Pen ($leafColorDark, ($Size * 0.05))
+        $stemPen = New-Object System.Drawing.Pen ($leafColorDark, ($Size * 0.042))
         $highlightBrush = New-Object System.Drawing.SolidBrush ([System.Drawing.Color]::FromArgb(56, 255, 255, 255))
 
         try {
             $graphics.FillEllipse($bodyBrush, $bodyRect)
             $graphics.DrawEllipse($outlinePen, $bodyRect)
             $graphics.FillPolygon($leafBrush, $leafPoints)
-            $graphics.DrawArc($stemPen, $Size * 0.45, $Size * 0.12, $Size * 0.18, $Size * 0.12, 210, 120)
-            $graphics.FillEllipse($highlightBrush, $Size * 0.26, $Size * 0.34, $Size * 0.22, $Size * 0.14)
-            $bitmap.Save($OutputPath, [System.Drawing.Imaging.ImageFormat]::Png)
+            $graphics.DrawArc($stemPen, $Size * 0.43, $Size * 0.07, $Size * 0.14, $Size * 0.11, 205, 130)
+            $graphics.FillEllipse($highlightBrush, $Size * 0.18, $Size * 0.32, $Size * 0.27, $Size * 0.15)
+
+            return $bitmap
         } finally {
             $bodyBrush.Dispose()
             $outlinePen.Dispose()
@@ -64,24 +64,51 @@ function New-TomatoIcon {
         }
     } finally {
         $graphics.Dispose()
+    }
+}
+
+function New-TomatoIcon {
+    param(
+        [int]$Size,
+        [string]$OutputPath
+    )
+
+    $bitmap = New-TomatoBitmap -Size $Size
+
+    try {
+        $bitmap.Save($OutputPath, [System.Drawing.Imaging.ImageFormat]::Png)
+    } finally {
         $bitmap.Dispose()
     }
 }
 
-function New-IcoFromPng {
+function New-TomatoPngBytes {
     param(
-        [string]$PngPath,
+        [int]$Size
+    )
+
+    $bitmap = New-TomatoBitmap -Size $Size
+    $memoryStream = New-Object System.IO.MemoryStream
+
+    try {
+        $bitmap.Save($memoryStream, [System.Drawing.Imaging.ImageFormat]::Png)
+        return $memoryStream.ToArray()
+    } finally {
+        $memoryStream.Dispose()
+        $bitmap.Dispose()
+    }
+}
+
+function New-IcoFromPngs {
+    param(
+        [object[]]$Images,
         [string]$IcoPath
     )
 
-    $pngBytes = [System.IO.File]::ReadAllBytes($PngPath)
-    $width = 0
-    $height = 0
     $bitCount = 32
-    $imageSize = $pngBytes.Length
     $headerSize = 6
     $directoryEntrySize = 16
-    $imageOffset = $headerSize + $directoryEntrySize
+    $imageOffset = $headerSize + ($directoryEntrySize * $Images.Count)
 
     $stream = [System.IO.File]::Open($IcoPath, [System.IO.FileMode]::Create, [System.IO.FileAccess]::Write)
     $writer = New-Object System.IO.BinaryWriter $stream
@@ -89,16 +116,28 @@ function New-IcoFromPng {
     try {
         $writer.Write([UInt16]0)
         $writer.Write([UInt16]1)
-        $writer.Write([UInt16]1)
-        $writer.Write([byte]$width)
-        $writer.Write([byte]$height)
-        $writer.Write([byte]0)
-        $writer.Write([byte]0)
-        $writer.Write([UInt16]1)
-        $writer.Write([UInt16]$bitCount)
-        $writer.Write([UInt32]$imageSize)
-        $writer.Write([UInt32]$imageOffset)
-        $writer.Write($pngBytes)
+        $writer.Write([UInt16]$Images.Count)
+
+        foreach ($image in $Images) {
+            $entryWidth = if ($image.Size -ge 256) { 0 } else { [byte]$image.Size }
+            $entryHeight = if ($image.Size -ge 256) { 0 } else { [byte]$image.Size }
+            $entryBytes = [byte[]]$image.Bytes
+
+            $writer.Write([byte]$entryWidth)
+            $writer.Write([byte]$entryHeight)
+            $writer.Write([byte]0)
+            $writer.Write([byte]0)
+            $writer.Write([UInt16]1)
+            $writer.Write([UInt16]$bitCount)
+            $writer.Write([UInt32]$entryBytes.Length)
+            $writer.Write([UInt32]$imageOffset)
+
+            $imageOffset += $entryBytes.Length
+        }
+
+        foreach ($image in $Images) {
+            $writer.Write([byte[]]$image.Bytes)
+        }
     } finally {
         $writer.Dispose()
         $stream.Dispose()
@@ -108,4 +147,10 @@ function New-IcoFromPng {
 $root = Split-Path -Parent $MyInvocation.MyCommand.Path
 New-TomatoIcon -Size 192 -OutputPath (Join-Path $root 'icon-192-v2.png')
 New-TomatoIcon -Size 512 -OutputPath (Join-Path $root 'icon-512-v2.png')
-New-IcoFromPng -PngPath (Join-Path $root 'icon-192-v2.png') -IcoPath (Join-Path $root 'favicon-v2.ico')
+$icoImages = @(16, 24, 32, 48, 64, 128, 256 | ForEach-Object {
+    [pscustomobject]@{
+        Size = $_
+        Bytes = New-TomatoPngBytes -Size $_
+    }
+})
+New-IcoFromPngs -Images $icoImages -IcoPath (Join-Path $root 'favicon-v2.ico')
